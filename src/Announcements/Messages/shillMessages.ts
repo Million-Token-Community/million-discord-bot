cb0ad8e476000f51e0ce8b432af05edc3de1e8d5
Peter Tran <tran.peter08@gmail.com>
2021-07-31 01:34:11 -0700

    created shill messages

---
+++ src/Announcements/Messages/shillMessages.ts
@@ -0,0 +1,19 @@
+export const shillMessages = [ 
+  `Checkout the charts :chart_with_upwards_trend: at DEX Tools:\n` +
+  'https://www.tinyurl.com/milliondextoolschart \n\n' +
+  `Don't forget to add to favorites :star:!`,
+
+  `CoinGecko :lizard: probably can't save you money on car insurance, ` +
+  `but they have some cool price charts :chart_with_upwards_trend:!\n` +
+  ' https://www.coingecko.com/en/coins/million \n\n' +
+  `And hit that star :star:!`,
+  
+  `What do you think Google has to say about Million Token :lion:?\n\n` +
+  `:point_right:  https://www.google.com/search?q=www.milliontoken.org`,
+
+  `I'm bored... let's watch something about Million Token :tv:!\n\n` + 
+  `:arrow_right:  https://www.youtube.com/results?search_query=million+token`,
+
+  'Why did you stop typing? Is it me?',
+
+];

0006dc0df0320f9175b569e76ae55eba25d6b5eb
Peter Tran <tran.peter08@gmail.com>
2021-07-31 01:34:02 -0700

    created recurring shills

---
+++ src/Announcements/RecurringShills.ts
@@ -0,0 +1,30 @@
+import {RecurringAnnouncement} from './RecurringAnnouncement';
+import {Client, TextChannel} from 'discord.js';
+import {randomInt} from '../utils';
+
+export class RecurringShills extends RecurringAnnouncement {
+  constructor(
+    client: Client,
+    channelId: string,
+    interval: number, 
+    private shills: string[],) {
+    super(client, channelId, interval, '');
+  }
+
+  async action(): Promise<void> {
+    try {
+      const channel = await this.client.channels.fetch(this.channelId) as TextChannel;
+    
+      if (!channel) {
+        throw 'Could not find channel with ID' + this.channelId;
+      }
+
+      const message = this.shills[randomInt(this.shills.length - 1)];
+
+      await channel.send(message);
+    } catch (error) {
+      console.log('Error creating announcement:', error);
+      clearInterval(this.timer);
+    }
+  }
+}

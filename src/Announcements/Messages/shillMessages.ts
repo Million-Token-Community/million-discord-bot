8dfc94ef2730c877ad1e4eb152a44af90e954d37
Peter Tran <tran.peter08@gmail.com>
2021-08-01 03:12:33 -0700

    more message updates, shoot me plz

--- src/Announcements/Messages/shillMessages.ts
+++ src/Announcements/Messages/shillMessages.ts
@@ -1,5 +1,5 @@
 export const shillMessages = [ 
-  `:chart_with_upwards_trend: Checkout the cool charts at DEX Tools:\n` +
+  `:chart_with_upwards_trend:  Check out the cool charts at DEX Tools:\n` +
   'https://www.tinyurl.com/milliondextoolschart \n\n' +
   `Don't forget to add to favorites :star:!`,
 
@@ -11,10 +11,10 @@ export const shillMessages = [
   `What do you think Google has to say about Million Token :lion:?\n\n` +
   `:point_right:  https://www.google.com/search?q=www.milliontoken.org`,
 
-  `Are you bored? Checkkout Million Token content on YouTube :tv:!\n\n` + 
+  `Are you bored? Checkk out Million Token content on YouTube :tv:!\n\n` + 
   `:arrow_right:  https://www.youtube.com/results?search_query=million+token`,
 
-  `Want to check out the price charts and do some TA for $MM? Checkout Defined.fi's awesome charts :chart_with_upwards_trend:!\n` +
+  `Want to check out the price charts and do some technical analysis for Million Token? Checkout Defined.fi's awesome charts :chart_with_upwards_trend:!\n` +
   `https://www.tinyurl.com/milliondefinedchart\n\n` +
   `:heart:  Give it a like to help boost $MM's ranking!`,
 
@@ -23,6 +23,11 @@ export const shillMessages = [
   `:bird:  Remember to like, retweet, and comment!`,
 
   `:moneybag:  Looking to buy some Million Token?  :moneybag:\n\n` +
-  `:point_right:  Buy from Uniswap <https://app.uniswap.org/#/swap?outputCurrency=0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611>\n\n` +
-  `:point_right:  Buy from PancakeSwap <https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xbf05279f9bf1ce69bbfed670813b7e431142afa4>\n`
+  `:point_right:  Buy from Uniswap:\n` + 
+  `<https://app.uniswap.org/#/swap?outputCurrency=0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611>\n\n` +
+  `:point_right:  Buy from PancakeSwap:\n` + 
+  `<https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xbf05279f9bf1ce69bbfed670813b7e431142afa4>`,
+
+  `:thinking:  Is Million Token legit or just another meme coin?  :thinking:\n\n` +
+  `:eyes:  See for yourself  :point_right:  https://www.certik.org/projects/milliontoken\n` +
 ];

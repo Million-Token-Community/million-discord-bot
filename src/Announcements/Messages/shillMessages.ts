d45c6fed39c7459f40cfc681dfd6ce1f476f5aa0
Peter Tran <tran.peter08@gmail.com>
2021-08-01 02:50:46 -0700

    fixed error

--- src/Announcements/Messages/shillMessages.ts
+++ src/Announcements/Messages/shillMessages.ts
@@ -22,7 +22,7 @@ export const shillMessages = [
   `https://twitter.com/hashtag/MillionToken\n\n` +
   `:bird:  Remember to like, retweet, and comment!`,
 
-  `:moneybag:  Looking to buy some :mm: $MM?  :moneybag:` +
+  `:moneybag:  Looking to buy some :mm: $MM?  :moneybag:\n` +
   `:point_right:  Buy from Uniswap <https://app.uniswap.org/#/swap?outputCurrency=0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611>\n` +
   `:point_right:  Buy from PancakeSwap <https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xbf05279f9bf1ce69bbfed670813b7e431142afa4>\n`
 ];

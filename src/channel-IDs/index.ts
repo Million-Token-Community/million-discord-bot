bff904c838bb9a82b99a4649b1ceec6a3bfa45d8
Peter Tran <tran.peter08@gmail.com>
2021-08-12 03:54:58 -0700

    added twitter channel ID

--- src/channel-IDs/index.ts
+++ src/channel-IDs/index.ts
@@ -1,5 +1,7 @@
 export const channelIds = {
   lounge: '870409423825276995',
   suggestion_box: '870438625219842090',
-  botCommandsChannel: '870759676848013332'
+  botCommandsChannel: '870759676848013332',
+  twitterStats: '875329757162639380',
+  // twitterStats: '875292559314915359'
 }

edf54d96dddd1f425bb72033a413eec279b7483a
Peter Tran <tran.peter08@gmail.com>
2021-08-12 16:28:39 -0700

    added reddit channel ID

--- src/channel-IDs/index.ts
+++ src/channel-IDs/index.ts
@@ -3,5 +3,5 @@ export const channelIds = {
   suggestion_box: '870438625219842090',
   botCommandsChannel: '870759676848013332',
   twitterStats: '875329757162639380',
-  // twitterStats: '875292559314915359'
+  redditStats: '875518239906598922'
 }

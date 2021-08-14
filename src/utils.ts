1644feba915adc64aad5612df7ed21defbfabc13
Rami Menai <rami.menai@outlook.com>
2021-08-14 22:23:03 +0300

    Fixed linting errors

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export function formatLargeNumber(number: number) {
+export function formatLargeNumber(number: number): string | number {
   const num = Math.abs(Number(number));
   return num >= 1.0e+9
     ? (num / 1.0e+9).toFixed(2) + "B"
@@ -9,7 +9,7 @@ export function formatLargeNumber(number: number) {
         : num;
 }
 
-export function formatPercentageChange(number: number) {
+export function formatPercentageChange(number: number): string {
   return `${(number > 0 ? '+' : '')}${(number * 100).toFixed(2)}`;
 }
 

b65fbf12a0a75e0192da22d346a40b9fb4442fa1
Peter Tran <tran.peter08@gmail.com>
2021-08-16 05:26:18 -0700

    added telegram channel ID

--- src/channel-IDs/index.ts
+++ src/channel-IDs/index.ts
@@ -6,4 +6,5 @@ export const channelIds = {
   redditStats: '875518239906598922',
   emailSubsChannel: '875776790734454805',
   holdersChannel: '875776895566876703',
+  telegramStats: '876802098333909022'  //  test server telegram channel ID
 }

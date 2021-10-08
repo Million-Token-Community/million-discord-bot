d8eff5a370a3a860a14eecb2dc97a7604660b078
Peter Tran <tran.peter08@gmail.com>
2021-10-08 13:55:23 -0700

    added dev bot-commands channel

--- src/channel-IDs/devChannelIds.ts
+++ src/channel-IDs/devChannelIds.ts
@@ -2,12 +2,13 @@ export const devChannelIds = {
   lounge: '882029588937572427',
   suggestion_box: '882029588937572429',
   botCommandsChannel: '882029589134725187',
-  twitterStats: '882029588769832961',
-  redditStats: '882029588769832960',
-  emailSubsChannel: '882029588769832962',
-  holdersChannel: '882029588585259035',
-  price: '882029588585259034',
-  telegramCount: '882029588585259037',
+  devBotCommandsChannel: '870759676848013332',
+  twitterStats: '875292559314915359',
+  redditStats: '875512962754961448',
+  emailSubsChannel: '875665191197618176',
+  holdersChannel: '875774832887857193',
+  price: '876778084865568869',
+  telegramCount: '876802098333909022',
   youTubePromotion: '885463886487252993',
   botCommand_DevChannel: '885463886487252993'
-}//Mario's IDs
+}

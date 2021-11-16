58d1d2d94ba2c04e8bf52e49a70d71d6f3c2a914
Peter Tran <tran.peter08@gmail.com>
2021-11-15 16:07:06 -0800

    updated reddit service

--- src/services/RedditService.ts
+++ src/services/RedditService.ts
@@ -1,18 +1,21 @@
-import * as Snoowrap from 'snoowrap';
+// import * as Snoowrap from 'snoowrap';
+import axios from 'axios';
 
 export class RedditService {
-  r: Snoowrap;
-  constructor() {
-    this.r = new Snoowrap({
-      userAgent: 'mm-bot/0001',
-      clientId: process.env.REDDIT_CLIENT_ID,
-      clientSecret: process.env.REDDIT_SECRET,
-      username: process.env.REDDIT_USERNAME,
-      password: process.env.REDDIT_PASSWORD
-    });
-  }
+  // r: Snoowrap;
+  // constructor() {
+  //   this.r = new Snoowrap({
+  //     userAgent: 'mm-bot/0001',
+  //     clientId: process.env.REDDIT_CLIENT_ID,
+  //     clientSecret: process.env.REDDIT_SECRET,
+  //     username: process.env.REDDIT_USERNAME,
+  //     password: process.env.REDDIT_PASSWORD
+  //   });
+  // }
 
-  async getMMSubCount(): Promise<number> {
-    return await this.r.getSubreddit('milliontoken').subscribers;
+  static async getMMSubCount(): Promise<number> {
+    const {data} = await axios.get('https://www.reddit.com/r/milliontoken/about.json');
+
+    return data.data.subscribers
   }
 }
